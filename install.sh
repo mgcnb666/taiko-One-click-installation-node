73b65320f6625bfa19002ec11ee4826fe0982d52
@OSSDA <80329840+mgcnb666@users.noreply.github.com>
2023-07-10 19:54:42 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
 
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
+sudo snap install docker
 
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo snap install docker-compose
 
 
-wget https://golang.org/dl/go1.16.6.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzf go1.16.6.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-source ~/.bashrc
+sudo snap install go --classic
 
 
-git clone https://github.com/ethereum/go-ethereum.git
-cd go-ethereum
-make geth
+sudo snap install geth
 
 
 git clone https://github.com/taikoxyz/simple-taiko-node.git
-
 cd simple-taiko-node
-
-
 cp .env.sample .env
+
+
